0c35ad77fb854dfe231f489e6bebd9c8dcc07e2a
c0dingfun <hackerrank888@gmail.com>
2020-03-21 08:54:15 -0700

    looping

--- HelloWorld/helloworld-template.go
+++ HelloWorld/helloworld-template.go
@@ -13,6 +13,7 @@ type User struct {
 	Name string
 }
 
+// Post struct
 type Post struct {
 	User User
 	Body string
@@ -30,10 +31,17 @@ func main() {
 	http.HandleFunc(
 		"/", // pattern
 		func(w http.ResponseWriter, r *http.Request) { // handler
-			user := User{Name: "kenny"}                            // instantiate User
-			vm := HelloViewModel{Title: "Home Page", User: user}   // instantiate VM
-			tpl, _ := template.ParseFiles("templates/hello2.html") // get the template
-			tpl.Execute(w, &vm)                                    // use the template
+			u1 := User{Name: "Kenny"}
+			u2 := User{Name: "Bob"}
+
+			posts := []Post{
+				Post{User: u1, Body: "Beautiful day in NY!"},
+				Post{User: u2, Body: "Beautiful Day in SF!"},
+			}
+
+			vm := HelloViewModel{Title: "Home Page", User: u1, Posts: posts} // instantiate VM
+			tpl, _ := template.ParseFiles("templates/hello3.html")           // get the template
+			tpl.Execute(w, &vm)                                              // use the template
 		})
 
 	// func ListenAndServe(addr string, handler Handler) error
